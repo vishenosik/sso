6f41975f652a64011fdb145515a4815ec49ad47f
vishenosik-lnx-home <n.kuznecov@russianit.ru>
2025-03-11 21:34:24 +0400

    test(test settings): Improve testing by correcting testing script.

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 COVERAGE_FILE=$1
-TESTING_LIST=$(go list ./... | grep -E "internal|pkg" | grep -v mocks )
+TESTING_LIST=$(go list ./... | grep -E "internal|pkg" | grep -v mocks | grep -v gen )
 
 go test -v -cover -coverprofile=$COVERAGE_FILE $TESTING_LIST
