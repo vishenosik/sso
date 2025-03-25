b784e7b8723afa6a9e9673172defb7eed2f58df0
vishenosik-lnx-home <n.kuznecov@russianit.ru>
2025-03-25 14:00:19 +0400

    feat(migrate fs): Add migrate schema migrations. Split fs from other functionality. Add iterators to fetching functions.

--- pkg/dgraph_migrator/version.go
+++ pkg/dgraph_migrator/version.go
@@ -10,12 +10,40 @@ import (
 	"github.com/vishenosik/sso/pkg/collections"
 )
 
+type Version struct {
+	Timestamp      string   `json:"version_timestamp,omitempty"`
+	CurrentVersion int64    `json:"version_current,omitempty"`
+	DType          []string `json:"dgraph.type,omitempty"`
+}
+
 type migration struct {
 	version  int64
 	filename string
 }
 
 type migrations = []migration
+
+func collectMigrations(filenames iter.Seq[string]) iter.Seq[migration] {
+	return func() iter.Seq[migration] {
+		return func(yield func(migration) bool) {
+			for filename := range filenames {
+				version, ok := parseVersion(filename)
+				if !ok {
+					continue
+				}
+
+				_migration := migration{
+					version:  version,
+					filename: filename,
+				}
+
+				if !yield(_migration) {
+					return
+				}
+			}
+		}
+	}()
+}
 
 func sortVersions(mgs migrations) {
 	slices.SortFunc(mgs, func(a, b migration) int {
